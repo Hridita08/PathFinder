6eec97e8e3cb3c8fc65964b961cdff242a99a9a6
Hridita08 <ghridita004@gmail.com>
2026-03-14 21:59:04 +0600

    updated login page

--- PathFinder Frontend/script.js
+++ PathFinder Frontend/script.js
@@ -3,6 +3,65 @@ function loginUser(event) {
     window.location.href = "home.html";
 }
 
+// login page
+function loginUser(e){
+
+e.preventDefault();
+
+let name = document.getElementById("name").value.trim();
+let email = document.getElementById("email").value.trim();
+let password = document.getElementById("password").value.trim();
+let error = document.getElementById("errorMsg");
+
+error.innerHTML="";
+
+if(name==="" || email==="" || password===""){
+error.innerHTML="All fields are required!";
+return;
+}
+
+let emailPattern=/^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+if(!email.match(emailPattern)){
+error.innerHTML="Please enter a valid email!";
+return;
+}
+
+if(password.length<6){
+error.innerHTML="Password must be at least 6 characters!";
+return;
+}
+
+localStorage.setItem("username",name);
+
+window.location.href="main.html";
+
+}
+function togglePassword(){
+
+const pass = document.getElementById("password");
+const icon = document.querySelector(".toggle-pass");
+
+if(pass.type === "password"){
+pass.type = "text";
+icon.classList.remove("fa-eye");
+icon.classList.add("fa-eye-slash");
+}
+else{
+pass.type = "password";
+icon.classList.remove("fa-eye-slash");
+icon.classList.add("fa-eye");
+}
+
+}
+window.addEventListener("DOMContentLoaded", () => {
+    const container = document.querySelector(".pf-login-container");
+    setTimeout(() => {
+        container.classList.add("show");
+    }, 100); // small delay for smoother effect
+});
+
+
 function addPost(){
     const input=document.getElementById("postInput");
     const feed=document.getElementById("feedArea");
