06839ab75a06a31377eb30780f888bde8fa93bfd
kazitaifa2284cyber <kazitaifa2284@gmail.com>
2026-04-10 13:06:47 +0600

    update app.py

--- PathFinder Frontend/app.py
+++ PathFinder Frontend/app.py
@@ -14,3 +14,21 @@ cursor.execute("SELECT * FROM users")
 
 for row in cursor:
     print(row)
+    from flask import Flask
+from flask_mysqldb import MySQL
+
+app = Flask(__name__)
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''   # jodi password thake, ekhane dao
+app.config['MYSQL_DB'] = 'pathfinder'
+
+mysql = MySQL(app)
+
+@app.route('/')
+def home():
+    return "Connected successfully!"
+
+if __name__ == '__main__':
+    app.run(debug=True)
